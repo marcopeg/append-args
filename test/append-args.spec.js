48c850ade4d9f995ccc4aa35f9e4283e69c07c12
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-01-19 12:04:23 +0100

    added tests

--- test/append-args.spec.js
+++ test/append-args.spec.js
@@ -4,8 +4,23 @@ import appendArgs from '../src/append-args';
 
 describe('appendArgs', () => {
 
+    // this will return the sum of all the given arguments
+    function sum() {
+        return [...arguments].reduce((pre, val) => pre + val, 0);
+    }
+
     it('should import the function', () => {
         expect(appendArgs).to.be.a('function');
+    });
+
+    it('shoud append one argument', () => {
+        var sum1 = appendArgs(1, sum);
+        expect(sum1(2)).to.equal(3);
+    });
+
+    it('shoud append many argument', () => {
+        var sumMany = appendArgs(1, 2, 3, 4, sum);
+        expect(sumMany(10)).to.equal(20);
     });
 
 });
